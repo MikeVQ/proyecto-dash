1b4253bc5c8cbb293a6f26dde4b36afb57f9262c
Mauro <sanmigueluio@outlook.com>
2025-02-28 12:21:04 -0500

    solucion UsuarioAsesor

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Asesor from "./components/Asesor";
-import UsuariosAsesor from "./components/UsuariosAsesor"; // <-- Importar tu nuevo componente
+import UsuarioAsesor from "./components/UsuarioAsesor"; // <-- Importar tu nuevo componente
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/asesor" element={<Asesor />} />
-        <Route path="/usuarios-asesor" element={<UsuarioAsesor />} /> 
+        <Route path="/usuario-asesor" element={<UsuarioAsesor />} /> 
         {/* ^^^ Nueva ruta */}
       </Routes>
     </Router>
