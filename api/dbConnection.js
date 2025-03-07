58de2049862d146d7384faffbf51805230b8c0f0
Mauro <sanmigueluio@outlook.com>
2025-03-07 10:14:34 -0500

    ds

--- api/dbConnection.js
+++ api/dbConnection.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   user: process.env.PG_USER || 'apiwebpostgres',
   password: process.env.PG_PASSWORD || 'AhApiWeb2023',
   database: process.env.PG_DATABASE || 'AdrianaHoyosDB',
+  ssl: {
+    rejectUnauthorized: false, // Esto permite conexiones SSL sin verificar el certificado
+  },
 });
 
 export const query = (text, params) => pool.query(text, params);
