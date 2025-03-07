85d7bdef1c141bcc9393505f6beb5618b399f642
Mauro <sanmigueluio@outlook.com>
2025-03-07 14:45:36 -0500

    ds

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // Las rutas serán relativas al directorio actual
   plugins: [react()],
 })
