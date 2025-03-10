73ef253b0e1780d30f9fdf1269522b49253f58ab
Mauro <sanmigueluio@outlook.com>
2025-03-09 19:23:27 -0500

    fsd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './', // Las rutas serán relativas al directorio actual
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Cambia este puerto al que uses para tu backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
